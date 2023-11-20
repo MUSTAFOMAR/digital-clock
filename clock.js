002c632bc4fc8d2f59ad1e8cea51929428f0c728
MUSTAFOMAR <149865850+MUSTAFOMAR@users.noreply.github.com>
2023-11-20 02:11:37 -0800

    Add files via upload

--- clock.js
+++ clock.js
@@ -15,7 +15,7 @@ function finishClock(){
   h = h<10 ? "0"+h :h;
   m = m<10 ? "0"+m :m;
   s = s<10 ? "0"+s :s;
-    ampmEL .innerText= ampm
+    ampmEL .innerText= ampm;
     hourEL .innerText =h;
     minuteEL .innerText= m;
     secondEL .innerText= s; 
